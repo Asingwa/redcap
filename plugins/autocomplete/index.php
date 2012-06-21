44be5321da929862d8b77e05ce0776d415006bf0
Fred McClurg <fred-mcclurg@uiowa.edu>
2012-06-21 11:50:25 -0500

    Remove hardcoded pathname

--- plugins/autocomplete/index.php
+++ plugins/autocomplete/index.php
@@ -3,21 +3,22 @@
     * @brief Performs autocompletion on an external REDCap project
     *
     * @file  jQueryDialogAutoComplete.php
-    * $Revision: 135 $
+    * $Revision: 147 $
     * $Author: fmcclurg $
-    * $Date:: 2012-05-25 16:11:25 #$
+    * $Date:: 2012-06-21 11:38:33 #$
     * @since 2012-05-14
     * $URL: https://srcvault.icts.uiowa.edu/repos/REDCap/REDCap/trunk/autocomplete/index.php $
     */
 
    // Call the REDCap Connect file in the main "redcap" directory
-   require_once "../redcap_connect.php";
+   require_once( "../redcap_connect.php" );
 
 	// common utility functions
 	require_once( "include/htmlFunctions.php" );
-?>
 
-<?php
+   // $dirName = dirname( $_SERVER['SCRIPT_NAME'] );
+   $dirName = dirname( $_SERVER['PHP_SELF'] );
+
    if ( ! isset( $_REQUEST['pid'] ) ||
         $_REQUEST['pid'] != PROJECT_ID )
    {
@@ -26,28 +27,28 @@
          window.alert( 'You do not have access to this project.' );
       </script>
 <?php
+   
       die( 'You do not have access to this project.' );
    }
 ?>
 
    <!-- CSS Style Sheets -->
-   <link href="/redcap/autocomplete/jquery/css/smoothness/jquery-ui-1.8.20.custom.css" type="text/css" rel="stylesheet" />
+   <link href="<?= $dirName ?>/jquery/css/smoothness/jquery-ui-1.8.20.custom.css" type="text/css" rel="stylesheet" />
 
    <style>
-      .ui-autocomplete-loading { background: white url('/redcap/autocomplete/jquery/development-bundle/demos/autocomplete/images/ui-anim_basic_16x16.gif') right center no-repeat; }
+      .ui-autocomplete-loading { background: white url('<?= $dirName ?>/jquery/development-bundle/demos/autocomplete/images/ui-anim_basic_16x16.gif') right center no-repeat; }
    </style>
 
    <!-- JavaScript Functions -->
-   <script src="/redcap/autocomplete/jquery/js/jquery-1.7.2.min.js" type="text/javascript" type="text/javascript"></script>
-   <script src="/redcap/autocomplete/jquery/js/jquery-ui-1.8.20.custom.min.js" type="text/javascript"></script>
-   <script src="/redcap/autocomplete/js/jsFunctions.js" type="text/javascript"></script>
+   <script src="<?= $dirName ?>/jquery/js/jquery-1.7.2.min.js" type="text/javascript" type="text/javascript"></script>
+   <script src="<?= $dirName ?>/jquery/js/jquery-ui-1.8.20.custom.min.js" type="text/javascript"></script>
 
 <?php
    $pfield = $_REQUEST['pfield'];
    $title = $_REQUEST['title'];
    $body = $_REQUEST['body'];
 
-   $scriptName = "/redcap/autocomplete/searchDatabase.php";
+   $scriptName = sprintf( "%s/searchDatabase.php", $dirName );
 	$queryString = BuildQueryString( $_REQUEST );
    $searchScript = sprintf( "%s?%s", $scriptName, $queryString );
 ?>
